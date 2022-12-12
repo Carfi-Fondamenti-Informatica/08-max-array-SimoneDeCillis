7108a1fc7e7dab8ac046be8d13ac07925f0c03a5
SimoneDeCillis <116493653+SimoneDeCillis@users.noreply.github.com>
2022-12-12 18:17:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int a;
+
+    cin >> a;
+    float array[a];
+    for (int i = 0; i < a; i++) {
+
+
+        cin >> array[i];
+    }
+
+
+    cout << max(array, a);
+
   return 0;
 }
